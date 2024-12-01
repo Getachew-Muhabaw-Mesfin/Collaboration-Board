fa9cce7359fd26445631d877f111356ce0934029
Getachew Muhabaw <getachewmuhabaw@gmail.com>
2024-12-01 21:12:51 +0300

    add initial entities and DTOs for posts, users, responses, categories, and notifications

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,45 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { ResponsesModule } from './responses/responses.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [],
+  imports: [
+    /**
+     * Database configuration with MySQL and TypeORM
+     * Load .env variables globally
+     */
+    ConfigModule.forRoot({ isGlobal: true }), // Load .env variables globally
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: true,
+        logging: true,
+        extra: {
+          connectTimeout: 30000,
+        },
+      }),
+    }),
+    UsersModule,
+    PostsModule,
+    NotificationsModule,
+    ResponsesModule,
+    CategoriesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
