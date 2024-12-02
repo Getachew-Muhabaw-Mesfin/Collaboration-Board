c8ca688afc4914cd07b836554d16fcbfaee15748
Getachew Muhabaw <getachewmuhabaw@gmail.com>
2024-12-02 23:44:06 +0300

    feat: update JWT auth guard documentation and clean up notifications module by removing unused imports

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -3,6 +3,7 @@ import { AuthGuard } from '@nestjs/passport';
 
 /**
  * JWT AUTH GUARD
+ * USE AUTH GUARD
  */
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {}
--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -2,10 +2,7 @@ import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
 import { ConfigModule } from '@nestjs/config';
-// import { PostsModule } from 'src/posts/posts.module';
-// import { UsersModule } from 'src/users/users.module';
-// import { ResponsesModule } from 'src/responses/responses.module';
-// import { CategoriesModule } from '../categories/categories.module';
+
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
 import { Notification } from './entities/notification.entity';
